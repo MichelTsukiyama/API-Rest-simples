46936862be8443f7030d83b455e0a77f1420aa81
Michel Tsukiyama <michel.tsukiyama@gmail.com>
2022-01-22 19:19:32 -0300

    adiciona API-Rest

---
+++ DIO.Series.Console/DIOO.Series.Web/Controllers/SerieController.cs
@@ -0,0 +1,54 @@
+﻿using DIO.Series.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DIO.Series.Web.Controllers
+{
+    [Route("[controller]")]
+    public class SerieController : Controller
+    {
+        private readonly IRepositorio<Serie> _repositorioSerie;
+
+        public SerieController(IRepositorio<Serie> repositorioSerie)
+        {
+            _repositorioSerie = repositorioSerie;
+        }
+
+        //GET = retornar algo; POST = Inserir algo; PUT = alterar algo; DELETE = excluir algo
+        [HttpGet("")]
+        public IActionResult Lista()
+        {
+            return Ok(_repositorioSerie.Lista().Select(s => new SerieModel(s)));
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualiza(int id, [FromBody] SerieModel model)
+        {
+            _repositorioSerie.Atualiza(id, model.ToSerie());
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Exclui(int id)
+        {
+            _repositorioSerie.Exclui(id);
+            return NoContent() ;
+        }
+
+        [HttpPost("")]
+        public IActionResult Insere (SerieModel model)
+        {
+            model.Id = _repositorioSerie.ProximoId();
+
+            Serie serie = model.ToSerie();
+
+            _repositorioSerie.Insere(model.ToSerie());
+            return Created("", model);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Consulta  (int id)
+        {
+            return Ok(new SerieModel(_repositorioSerie.Lista().FirstOrDefault(s => s.Id == id)));
+        }
+    }
+}
